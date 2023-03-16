e03426448651ac801e8a11cf79aee955b4eb19cb
Diogo Guedes <diogofoguede@hotmail.com>
2023-03-16 16:21:04 +0000

    version 1

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const app = express(); // start an express app
+const port = process.env.PORT || 5501;
+const path = require("path"); // window or mac
+const router = express.Router();
+
+app.use(express.static(".")); // sending static files 
+
+
+app.use("/", router);
+
+
+app.listen(port, () => {
+    console.log("App is starting at port ", port);
+}); // required
+
+
+
+router.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+
+router.get("*", (req, res) => {
+    res.json("Page not found");
+});
+
+
+
